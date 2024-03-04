3c87cb0cccd5868a346c4a200173684f972b490e
Senthil Palanivelu <spalanivelu@mgh.harvard.edu>
2024-03-04 16:12:29 -0500

    working

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
+  output: 'export',
   reactStrictMode: true,
   images:{
     loader:"akamai",
+    unoptimized: true,
     path:"",
   }
 }
