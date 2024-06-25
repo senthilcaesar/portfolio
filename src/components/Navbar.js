46880882145983aa68ec9159c31e2916c4ad3f8b
Senthil Palanivelu <spalanivelu@mgh.harvard.edu>
2024-06-25 15:26:32 -0400

    Dropbox Updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -131,7 +131,7 @@ const Navbar = () => {
         */}
 
           <motion.a
-            href="https://www.dropbox.com/scl/fi/3inqmmrh1o4jpieke4om0/Senthil-Resume.pdf?rlkey=oj4xijvon6pif9cddw0n1nnzq&st=p2xdbjdj&dl=0"
+            href="https://www.dropbox.com/scl/fi/3inqmmrh1o4jpieke4om0/Senthil-Resume.pdf?rlkey=oj4xijvon6pif9cddw0n1nnzq&st=lv84la2z&dl=0"
             className="mr-80 cursor-pointer w-max origin-center absolute 
                   font-semibold bg-dark text-light py-1 px-3 rounded-full dark:bg-light dark:text-dark
                   lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3  xs:bg-transparent xs:dark:bg-transparent xs:text-dark xs:dark:text-light xs:font-bold
